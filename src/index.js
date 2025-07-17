7a26c98df6596651aa1a9ca53f8cc926456be1ab
Your Name <your.email@example.com>
2025-07-17 17:52:18 +0300

    Добавлен автоматический трекинг просмотров страниц для Яндекс Метрики (SPA)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function YandexMetrikaHit() {
+  const location = useLocation();
+  useEffect(() => {
+    if (window.ym) {
+      window.ym(103368921, 'hit', location.pathname + location.search);
+    }
+  }, [location]);
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <YandexMetrikaHit />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
